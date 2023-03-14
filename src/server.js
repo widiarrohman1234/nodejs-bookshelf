02402afb28cbc6f14bce0d73cfbc910472a1c418
Widi Arrohman <widiarrohman1234@gmail.com>
2023-03-14 19:07:09 +0700

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,20 +1,25 @@
 const Hapi = require("@hapi/hapi");
 const routes = require("./routes");
 
-const init = async ()=>{
-    const server= Hapi.server({
-        port: 9000,
-        host: 'localhost',
-        routes: {
-            cors: {
-                origin: ["*"]
-            }
-        }
-    })
+const init = async () => {
+  const server = Hapi.server({
+    port: 5000,
+    host: "localhost",
+  });
 
-    //route
-    server.route(routes.routes)
-    await server.start()
-    console.log(`Running Server ${server.info.uri}`)
-}
-init()
+  server.route({
+    method: "GET",
+    path: "/",
+    handler: (request, h) => {
+      return "Hello World!";
+    },
+  });
+
+  await server.start();
+  console.log("Server running on %s", server.info.uri);
+};
+process.on("unhandledRejection", (err) => {
+  console.log(err);
+  process.exit(1);
+});
+init();
