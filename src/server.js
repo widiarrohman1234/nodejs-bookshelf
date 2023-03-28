6baa1e93384b9662442d0d6704d09289e421055e
Widi Arrohman <widiarrohman1234@gmail.com>
2023-03-28 13:16:04 +0700

    Update server.js

--- src/server.js
+++ src/server.js
@@ -11,7 +11,7 @@ const init = async () => {
     method: "GET",
     path: "/",
     handler: (request, h) => {
-      return "Hello World!";
+      return "Hello World!, oh yeaahhh";
     },
   });
 
