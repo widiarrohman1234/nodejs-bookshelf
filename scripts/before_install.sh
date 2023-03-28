d90249e881fe106e29eef830f401a47d5d8d56d3
Widi Arrohman <widiarrohman1234@gmail.com>
2023-03-28 11:10:37 +0700

    1

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -14,7 +14,7 @@ if [ ! command -v nvm &> /dev/null ]; then
 fi
 
 # CEK DIREKTORI
-DIR="~/express-app"
+DIR="/home/ec2-user/express-app"
 echo "CEK DIRECTORY $DIR"
 if [ -d "$DIR" ]; then
   echo "$DIR TELAH ADA"
